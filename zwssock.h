ffa795188ba2e6b0bb188f782baebfd6b5af6a2a
Ladislav Sopko <lsopko@3di.it>
2016-06-03 06:44:25 +0200

    inconsistent link on win

--- zwssock.h
+++ zwssock.h
@@ -4,23 +4,35 @@
 #include <czmq.h>
 
 
+#if defined (__WINDOWS__)
+#   if defined zwssock_STATIC
+#       define ZWSOCK_EXPORT
+#   elif defined zwssock_EXPORTS
+#       define ZWSOCK_EXPORT __declspec(dllexport)
+#   else
+#       define ZWSOCK_EXPORT __declspec(dllimport)
+#   endif
+#else
+#   define ZWSOCK_EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct _zwssock_t zwssock_t;
 
-CZMQ_EXPORT zwssock_t* zwssock_new_router(zctx_t *ctx);
+ZWSOCK_EXPORT zwssock_t* zwssock_new_router(zctx_t *ctx);
 
-CZMQ_EXPORT void zwssock_destroy(zwssock_t **self_p);
+ZWSOCK_EXPORT void zwssock_destroy(zwssock_t **self_p);
 
-CZMQ_EXPORT int zwssock_bind(zwssock_t *self, char *endpoint);
+ZWSOCK_EXPORT int zwssock_bind(zwssock_t *self, char *endpoint);
 
-CZMQ_EXPORT int zwssock_send(zwssock_t *self, zmsg_t **msg_p);
+ZWSOCK_EXPORT int zwssock_send(zwssock_t *self, zmsg_t **msg_p);
 
-CZMQ_EXPORT zmsg_t * zwssock_recv(zwssock_t *self);
+ZWSOCK_EXPORT zmsg_t * zwssock_recv(zwssock_t *self);
 
-CZMQ_EXPORT void* zwssock_handle(zwssock_t *self);
+ZWSOCK_EXPORT void* zwssock_handle(zwssock_t *self);
 
 #ifdef __cplusplus
 }
